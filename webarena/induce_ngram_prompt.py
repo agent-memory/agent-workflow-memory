b36e7aea7a60d05ba3c6932a886006ec498669c5
Vishwa Shah <52594777+vishwa27yvs@users.noreply.github.com>
2024-11-18 17:32:40 -0500

    Create induce_ngram_prompt.py
    Initial code for n-gram model based validation

---
+++ webarena/induce_ngram_prompt.py
@@ -0,0 +1,323 @@
+import os
+import json
+import random
+import argparse
+import re
+
+import openai
+from openai import OpenAI
+from huggingface_hub import InferenceClient
+
+
+def load_blocks(path: str) -> list[list[str]]:
+    """Load blank-line separated blocks from the log file."""
+    blocks, block = [], []
+    for line in open(path, 'r'):
+        if line.strip() == "":
+            blocks.append(block)
+            block = []
+        else:
+            if line.strip():
+                block.append(line.strip())
+    if len(blocks) > 0 and 'Python version' in blocks[0][0]:
+        blocks = blocks[1:] # remove conda env output
+    assert len(blocks) % 2 == 0
+    return blocks
+
+def remove_invalid_steps(actions: list[str]) -> list[str]:
+    """Remove invalid steps from the action sequence."""
+    valid_actions = []
+    for a in actions:
+        if "click(" in a:
+            arg = a[a.index("(")+1: a.index(")")]
+            try:
+                if type(eval(arg)) == str and type(eval(arg[1:-1])) == int:
+                    valid_actions.append(a)
+            except:
+                continue
+        elif "fill(" in a:
+            arg = a[a.index("(")+1: a.index(",")].strip()
+            if type(eval(arg)) == str:
+                valid_actions.append(a)
+        else:
+            valid_actions.append(a)
+    return valid_actions
+
+def extract_think_and_action(path: str) -> tuple[list[str], list[str]]:
+    """Extract the task trajectory from the log file."""
+    blocks = load_blocks(path)
+    think_list, action_list = [], []
+    for i in range(1, len(blocks), 2):
+        # action
+        b = blocks[i]
+        actions = remove_invalid_steps(b[1:])
+        if len(actions) == 0: continue
+        action_list.append(actions)
+        # think
+        b = blocks[i-1]
+        idx = b[-1].index("browsergym.experiments.loop - INFO -")
+        think_list.append(b[-1][idx+36: ].strip())
+    
+    assert len(think_list) == len(action_list)
+    
+    # TODO: merge same actions
+    return think_list, action_list
+
+def get_abstract_actions(action_list: list[list[str]]) -> str:
+    # Remove the arguments
+    abstract = []
+    for acts in action_list:
+        for a in acts:
+            s = a.index("(")
+            e = a.index(',', s) if ',' in a[s:] else a.index(")", s) # Only uses the first argument of the input (eg: fill has 2)
+            action = a[:s]
+            if action != "send_msg_to_user":
+                arg = a[s+1: e]
+                abstract.append(f"{action}({arg})")
+            else:
+                abstract.append(f"{action}")
+    # Need 1:1 corresp between abstract actions and overall actions
+    return abstract
+
+def generate_ngrams(abstract_action_list, n):
+    abstract_ngrams = []
+    ngram_idxs = []
+    # action_ngrams = []
+    # Loop through the actions to generate unique n-gram action sequences
+    
+    for i in range(len(abstract_action_list) - n + 1):
+        abs_ngram = abstract_action_list[i:i+n]
+        if abs_ngram not in abstract_ngrams:
+            abstract_ngrams.append(abs_ngram)
+            ngram_idxs.append(list(range(i,i+n)))
+    return abstract_ngrams, ngram_idxs
+
+def get_n_gram_action_sequences(abstract_action_list, n_range =[2,3,4,5]):
+    # n-gram creation based on abstract, store actions
+    n_gram_dict = {}
+    for n in n_range:
+        abs_ngram, ngram_idxs = generate_ngrams(abstract_action_list, n)
+        n_gram_dict[n] = (abs_ngram, ngram_idxs)
+    return n_gram_dict
+
+def llm_validate_subtrajectory(llm_client, subtraj, examples, args, verbose: bool = False):
+    """Call gpt model to validate if sub-trajectory is correct and provide explanation."""
+
+    steps = "/n".join(subtraj.split(",")) # Abstract Sub-trajectory that we want to validate
+    query_format = f"Sub-trajectory query sequence:\n{steps}\n\nActual trajectory:\n{examples}"
+
+    prompt = '\n\n'.join([args.INSTRUCTION, args.ONE_SHOT])
+
+    if verbose: print("Prompt:\n", prompt, '\n\n')
+
+    print(f"Model called for induction: {args.model}")
+    if "gpt" in args.model:
+        response = llm_client.chat.completions.create(
+            model=args.model,
+            messages=[{"role": "user", "content": prompt}],
+            temperature=1.0,
+            max_tokens=1024,
+        )
+    else:
+        response = llm_client.chat_completion(
+                messages=[{"role": "user", "content": prompt}],
+                temperature=1.0,
+                max_tokens=2048,
+        )
+
+    response = response.choices[0].message.content
+    
+    inducted_workflow = None
+    # Response parts: 0 - Validity 1- Explanation 2- NL Query 3- Example subtraj
+    validity_match = re.search(r"<VALIDITY>\s*(\w+)", response)
+    validity = validity_match.group(1) if validity_match else None
+    is_valid = True if "True" in validity else False
+    if is_valid:
+        query_match = re.search(r"<NATURAL_LANG_QUERY>\s*(.*?)<SUBTRAJECTORY_EXAMPLE_CALL>", response, re.DOTALL)
+        nl_query = query_match.group(1).strip() if query_match else None
+        
+        subtrajectory_examples = response.split("<SUBTRAJECTORY_EXAMPLE_CALL>")[-1]
+        subtrajectory_examples = "\n".join([x.strip() for x in subtrajectory_examples.split("\n")])
+        
+        inducted_workflow = f"Workflow: {nl_query}\n\nSubtrajectory Example:{subtrajectory_examples}\n"
+    if verbose: 
+        print("=====================") 
+        print(response)
+    return validity, inducted_workflow
+
+def main():
+    # collect result directories, e.g., ["results/webarena.0", ...] 
+    args.result_dir = args.result_dir.split()
+    if args.criteria == "gt":
+        file_dirs = [
+            os.path.join(res_dir, f) for res_dir in args.result_dir for f in os.listdir(res_dir) 
+            if json.load(
+                open(os.path.join(res_dir, f, "summary_info.json"))
+            )["cum_reward"]
+        ]
+    elif args.criteria == "autoeval":
+        file_dirs = []
+        for res_dir in args.result_dir:
+            for f in os.listdir(res_dir):
+                # Adding all paths irrespective of autoeval trajectory
+                # record_path = os.path.join(res_dir, f, f"{args.model}_autoeval.json")
+                # if not os.path.exists(record_path): continue
+                # record = json.load(open(record_path))
+                # try:
+                #     if record[0]["rm"]:
+                #         file_dirs.append(os.path.join(res_dir, f))
+                # except:
+                #     print(f"Autoeval format issue: tid {f}")
+                file_dirs.append(os.path.join(res_dir, f))
+    else:
+        raise ValueError(f"Invalid criteria: {args.criteria}.")
+    
+    print(f"Collected {len(file_dirs)} result directories.")
+
+    # template id based deduplication
+    template_dict = {}
+    all_task_action_think = {}
+    for f in file_dirs:
+        # get query -> task objective
+        task_id = f.split('/')[-1].split("_")[0].split(".")[1]
+        config_path = os.path.join("config_files", f"{task_id}.json")
+        config = json.load(open(config_path))
+        query = config["intent"]
+
+        # template_id = config["intent_template_id"] # for deduplication
+
+        # parse trajectory
+        log_path = os.path.join(f, "experiment.log")
+        try:
+            think_list, action_list = extract_think_and_action(log_path)
+        except:
+            think_list, action_list = [],[]
+
+        # add to template dict
+        abstract_traj = get_abstract_actions(action_list)
+
+        action_ngrams_dict = get_n_gram_action_sequences(abstract_traj) # indices are used to index original thought and action pairs
+        wdict = {"task_id":task_id,"query": query, "think_list": think_list, "action_list": action_list,
+                 "abstract_traj": abstract_traj, "action_ngrams": action_ngrams_dict}
+        all_task_action_think[task_id] = wdict
+
+    # test_n = 3
+    if len(file_dirs) < 60:
+        test_n = 2
+    else:
+        test_n = 3
+    print("N in n-gram:{}".format(test_n))
+
+    test_n = 2
+    ngram_groups = {}
+    # Count overlapping ngrams - for each ngram store the task id and the corresponding action sequence
+    for tid in all_task_action_think:
+        trajectory_log = all_task_action_think[tid]
+        ngrams = trajectory_log['action_ngrams'][test_n]
+        if len(ngrams) != 0: # If ngrams exist
+            for ngram_action, n_gram_idx in zip(ngrams[0], ngrams[1]):
+                ngram_key = ", ".join(ngram_action) # using the abstract action list
+                if ngram_key not in ngram_groups:
+                    ngram_groups[ngram_key] = []
+                ngram_groups[ngram_key].append({'task_id':trajectory_log['task_id'], 'ngram_idxs':n_gram_idx})
+
+    ngram_freq = {}
+    for key in ngram_groups.keys():
+        task_ids = [task['task_id'] for task in ngram_groups[key]]
+        ngram_freq[key] = len(task_ids)
+        print(f"Ngram:{key}, freq:{ngram_freq[key]}")
+
+    def format_trajectory(think_list: list[str], action_list: list[list[str]]) -> str:
+        trajectory = []
+        for t, a in zip(think_list, action_list):
+            acts = '\n'.join(a)
+            trajectory.append(f"<think>\n{t}\n</think>\n<action>\n{acts}\n</action>")
+        return '\n\n'.join(trajectory)
+
+    workflows = []
+    # 2-gram followed by 3-gram with increasing thr
+    if len(file_dirs) < 60:
+        induction_thr = 10
+    else:
+        induction_thr = 20
+
+    print("Induction threshold:{}".format(induction_thr))
+
+    if "gpt" in args.model:
+        openai.api_key = os.environ["OPENAI_API_KEY"]
+        llm_client = OpenAI()
+    else:
+        llm_client = InferenceClient(args.model, token = os.environ.get('HF_TOKEN'))
+
+    ## TODO: Load N-grams of each n, and check if the validation output is cached or not
+    ## TODO: Right now checking only based on the format in 1 trajectory
+    ctr = 0
+    for key in ngram_groups.keys():
+        if ngram_freq[key] >= induction_thr:
+            random_sample = random.sample(ngram_groups[key], 1) # should be less than induction_thr
+            examples_strs = []
+            for task in random_sample:
+                tid = task['task_id']
+                think_list = all_task_action_think[tid]['think_list']
+                action_list = all_task_action_think[tid]['action_list']
+                task_action_paired_str = format_trajectory(think_list, action_list)
+                examples_strs.append(task_action_paired_str)
+
+            example_traj = "\n".join(examples_strs)
+            is_valid, inducted_workflow = llm_validate_subtrajectory(llm_client, key, examples_strs, args, verbose=True)
+
+            print(is_valid)
+            print("***************")
+            print(inducted_workflow)
+
+            # workflow_formatted = get_workflow(ngram_format, examples_strs)
+            # workflows.append(workflow_formatted)
+            break
+            # ctr += 1
+
+    return 
+
+    print(f"#{len(workflows)} result dirs for ngram based selection..")
+
+    if args.output_path is None:
+        website = config["sites"][0]  # assumes all results are about the same website
+        args.output_path = f"workflow/{website}.txt"
+        print(f"[Warning] no output path specified, using '{args.output_path}' by default")
+        
+    with open(args.output_path, 'w') as fw:
+        fw.write('\n\n\n'.join(["## Subtrajectory Examples"] + workflows))
+
+    if not os.path.exists('step_wise_workflows'):
+        os.makedirs('step_wise_workflows')
+        print(f"Directory 'step_wise_workflows' created.")
+
+    step_wise_workflow_dir = os.listdir(f'step_wise_workflows')
+    if len(step_wise_workflow_dir) == 0:
+        step_idx = 0
+    else:
+        step_ids = [int(workflow_dir.split('workflow_step_')[-1].split('.txt')[0]) for workflow_dir in step_wise_workflow_dir]
+        last_idx = sorted(step_ids)[-1]
+        step_idx = last_idx + 1
+
+    with open(f'step_wise_workflows/workflow_step_{step_idx}.txt','w') as fw:
+        fw.write('\n\n\n'.join(["## Subtrajectory Examples"] + workflows))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--result_dir", type=str, default="results",
+                        help="Path to the result directory. Support multiple directories separated by space.")
+    parser.add_argument("--output_path", type=str, default=None,
+                        help="Path to the output file.")
+    parser.add_argument("--criteria", type=str, default="autoeval", 
+                        choices=["gt", "autoeval"],
+                        help="'gt': only use examples with gold reward, 'autoeval': use examples with autoeval reward.")
+    parser.add_argument("--model", type=str, default="gpt-4o",
+                        choices=["gpt-3.5", "gpt-4", "gpt-4o","gpt-4-turbo",
+                                 "meta-llama/Meta-Llama-3.1-70B-Instruct","meta-llama/Meta-Llama-3.1-8B-Instruct"])
+    parser.add_argument("--auto", action="store_true", help="w/o manual workflow inspections.")
+    args = parser.parse_args()
+
+    args.INSTRUCTION = open("prompt/instruction_subtraj.txt", 'r').read()
+    args.ONE_SHOT = open("prompt/one_shot_subtraj.txt", 'r').read()
+
+    main()
